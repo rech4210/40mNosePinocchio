05fd46391331bd4a421348731465944590b375d8
Moru-Mooseong <antjd0008@naver.com>
2022-10-22 02:02:54 +0900

    외부에셋 : 오딘인스펙터 추가 플레이어 데이터 업데이트

--- Assets/Moru/Scripts/TestScripts/TestPlayerDataLoad.cs
+++ Assets/Moru/Scripts/TestScripts/TestPlayerDataLoad.cs
@@ -9,28 +9,37 @@ public class TestPlayerDataLoad : MonoBehaviour
     void Start()
     {
         data = PlayerData.instance;
-        data.Load_GameData();
+        PlayerData.Load_GameData();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            PlayerData.onClearGame?.Invoke(GAME_INDEX.Snow_White);
-        }
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            PlayerData.onClearGame?.Invoke(GAME_INDEX.Cinderella);
+            PlayerData.onClearGame?.Invoke(GAME_INDEX.Cinderella, 0);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            PlayerData.onClearGame?.Invoke(GAME_INDEX.Pinocchio);
+            PlayerData.onClearGame?.Invoke(GAME_INDEX.Cinderella, 1);
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            PlayerData.onClearGame?.Invoke(GAME_INDEX.Little_Mermaid);
+            PlayerData.onClearGame?.Invoke(GAME_INDEX.Cinderella, 2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            PlayerData.onClearGame?.Invoke(GAME_INDEX.Cinderella, 3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var value = PlayerData.GetStageClearDataPerGame(GAME_INDEX.Cinderella);
+            for (int i = 0; i < value.Length; i++)
+            {
+                Debug.Log($"{value[i]}");
+            }
         }
     }
 }
