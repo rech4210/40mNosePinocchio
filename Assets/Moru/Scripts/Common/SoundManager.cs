a82d3e28118c22d11e842391110bc6969365b5d2
Moru-Mooseong <antjd0008@naver.com>
2022-10-23 02:33:17 +0900

    사운드매니저 및 옵션 제작완료

--- Assets/Moru/Scripts/Common/SoundManager.cs
+++ Assets/Moru/Scripts/Common/SoundManager.cs
@@ -4,5 +4,38 @@ using UnityEngine;
 
 public class SoundManager : SingleToneMono<SoundManager>
 {
-    
+    public AudioSource BGMs;
+    public AudioSource SFXs;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        var comps = new List<AudioSource>();
+        for(int i = 0; i < 2; i++)
+        {
+            comps.Add(this.gameObject.AddComponent<AudioSource>());
+        }
+        BGMs = comps[0];
+        SFXs = comps[1];
+    }
+
+    /// <summary>
+    /// 해당 BGM을 재생합니다.
+    /// </summary>
+    /// <param name="clip"></param>
+    public static void PlayBGM(AudioClip clip)
+    {
+        var instance = SoundManager.Instance;
+        instance.BGMs.clip = clip;
+        instance.BGMs.Play();
+    }
+
+    public static void PlaySFX(AudioClip clip)
+    {
+        var instance = SoundManager.Instance;
+        instance.SFXs.PlayOneShot(clip);
+        
+    }
+
+
 }
