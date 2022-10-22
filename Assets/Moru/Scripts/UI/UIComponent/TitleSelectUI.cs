bdcdf614a51f1ff3f880cbd2f39c3a7babeeb893
Moru-Mooseong <antjd0008@naver.com>
2022-10-22 12:46:50 +0900

    기획상 나온 UI 작업은 모두 완료.... 기능 테스트 이전에 잠시 밥타임

---
+++ Assets/Moru/Scripts/UI/UIComponent/TitleSelectUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSelectUI : MonoBehaviour
+{
+    [SerializeField] Transform contents;
+    [SerializeField] GameObject prefaps;
+    [SerializeField] StackUIComponent popUpUI;
+
+    void Start()
+    {
+        for (int i = 0; i < (int)ACHEIVE_INDEX.NONE; i++)
+        {
+            var obj = Instantiate(prefaps, contents);
+            obj.GetComponent<PlayerTitleUI>().Init(PlayerData.instance.Cur_AchievementValue[(ACHEIVE_INDEX)i], popUpUI);
+        }
+    }
+
+    
+}
